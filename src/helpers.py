3d0252c751d654ef4ceb00f77d6408559dd817ad
Kiran Ikram <kiran.ikram91@gmail.com>
2022-10-19 14:54:24 +0100

    added norm helper for next exp

--- src/helpers.py
+++ src/helpers.py
@@ -1,7 +1,6 @@
 import numpy as np
 import torch
 import math
-
 import sys
 
 
@@ -22,13 +21,12 @@ def make_regularized_pca_loss_X(lambd: float, *, norm: int = 2):
         assert n_x == n
         assert m_y == m
 
-        return (
-            torch.norm(A - X @ Y) ** 2 / (n * m)
-            + lambd * torch.norm(X, norm) ** norm / (n * k)
-            
-        )
+        return torch.norm(A - X @ Y) ** 2 / (n * m) + lambd * torch.norm(
+            X, norm
+        ) ** norm / (n * k)
 
     return loss
+
 
 def make_regularized_pca_loss_Y(lambd: float, *, norm: int = 2):
     """Sets up the objective function
@@ -47,10 +45,9 @@ def make_regularized_pca_loss_Y(lambd: float, *, norm: int = 2):
         assert n_x == n
         assert m_y == m
 
-        return (
-            torch.norm(A - X @ Y) ** 2 / (n * m)
-            + lambd * torch.norm(Y, norm) ** norm / (m * k)
-        )
+        return torch.norm(A - X @ Y) ** 2 / (n * m) + lambd * torch.norm(
+            Y, norm
+        ) ** norm / (m * k)
 
     return loss
 
@@ -78,3 +75,40 @@ def SVD_initialization(A, rank):
     Y_init.requires_grad = True
 
     return X_init, Y_init
+
+
+def get_matrix_norm(matrix: tensor, norm: int, use_torch: bool = True):
+    """Returns norm for matrix as defined by user.
+        norm of a vector is "the size or length of a vector is a
+        nonnegative number that describes the extent of the vector
+        in space, and is sometimes referred to as the vector’s
+        magnitude or the norm.
+
+        1-Norm is "the sum of the absolute vector values, where the
+        absolute value of a scalar uses the
+        notation |a1|. In effect, the norm is a calculation of the
+        Manhattan distance from the origin of the vector space.
+
+        2-Norm is "the distance of the vector coordinate from
+        the origin of the vector
+        space. The L2 norm is calculated as the square root
+        of the sum of the squared vector values."
+
+
+
+    Args:
+        matrix (tensor): input matrix
+        norm (int): 0 , 1 or 2 norm
+
+    """
+
+    if use_torch:
+        return torch.linalg.norm(matrix, dim=1, ord=norm)
+
+    else:
+        if norm == 0:
+            return x.ne(0).sum(dim=1)
+        elif norm == 1:
+            return x.abs().sum(dim=1)
+        elif norm == 2:
+            return x.pow(2).sum(dim=1).sqrt()
